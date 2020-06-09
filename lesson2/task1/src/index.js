b28a979f9079aa95cb8c610bd03da2accf3f8b04
John Doe <vperedz@gmail.com>
2020-06-09 14:27:49 +0300

    add

--- lesson2/task1/src/index.js
+++ lesson2/task1/src/index.js
@@ -3,13 +3,13 @@ import { createStore } from 'redux';
 const INCREMENT = 'COUNTER/INCREMENT';
 const DECREMENT = 'COUNTER/DECREMENT';
 
-export const incrementActionCreator = () => {
+export const increment = () => {
     return {
         type: INCREMENT,
     };
 };
 
-export const decrementActionCreator = () => {
+export const decrement = () => {
     return {
         type: DECREMENT,
     };
