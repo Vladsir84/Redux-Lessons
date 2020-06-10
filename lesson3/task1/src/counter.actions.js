a5542182305be71c3b0355b8fd2b586872d0ff81
John Doe <vperedz@gmail.com>
2020-06-10 18:19:54 +0300

    add

--- lesson3/task1/src/counter.actions.js
+++ lesson3/task1/src/counter.actions.js
@@ -1,8 +1,6 @@
-import { createStore } from 'redux';
-
-const INCREMENT = 'COUNTER/INCREMENT';
-const DECREMENT = 'COUNTER/DECREMENT';
-const RESET = 'COUNTER/RESET';
+export const INCREMENT = 'COUNTER/INCREMENT';
+export const DECREMENT = 'COUNTER/DECREMENT';
+export const RESET = 'COUNTER/RESET';
 
 export const increment = () => {
     return {
@@ -22,35 +20,5 @@ export const reset = () => {
     return {
         type: RESET,
     };
-}
-
-const initialState = {
-    history: [],
 };
 
-const counterReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case INCREMENT:
-            return {
-                ...state,
-                history: state.history.concat(1)
-            };
-        case DECREMENT:
-            return {
-                ...state,
-                history: state.history.concat(-1)
-            };
-        case RESET:
-            return {
-                ...state,
-                history: []
-            };
-        default:
-            return state;
-
-    }
-};
-
-const store = createStore(counterReducer);
-
-export default store;
