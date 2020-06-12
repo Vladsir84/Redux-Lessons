ba1489ba4851cf74cf8f3240f2ca6ba9a1449253
John Doe <vperedz@gmail.com>
2020-06-12 14:57:08 +0300

    add

---
+++ lesson4/task3/src/users/UsersList.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import User from './User'
+import Pagination from './Pagination';
+import * as userActions from './users.actions'
+
+
+class UsersList extends Component {
+     
+    render() {
+        const { users, goPrev, goNext, currentPage } = this.props;
+        const itemsPerPage = 3;
+        const start = currentPage * itemsPerPage;
+        const usersList = users.slice(start, start + itemsPerPage);
+        
+        return (
+            <div>
+                <Pagination
+                    goPrev={goPrev}
+                    goNext={goNext}
+                    currentPage={currentPage}
+                    totalItems={users.length}
+                    itemsPerPage={itemsPerPage}
+                    />
+                <ul className="users">
+                {usersList.map(user => (
+                    <User key={user.id} {...user} />
+                    ))}
+                </ul>
+            </div>
+        );
+    }
+}
+
+
+const mapState = state => {
+    return {
+        users: state.usersList,
+        currentPage: state.currentPage,
+    }
+};
+
+const mapDispatch = {
+
+    goPrev: userActions.goPrev,
+    goNext: userActions.goNext,
+
+};
+
+const connector = connect(mapState, mapDispatch);
+const ConnectedUsersList = connector(UsersList);
+
+export default ConnectedUsersList;
---
+++ lesson4/task3/src/users/user.reducer.js
@@ -0,0 +1,76 @@
+import { GO_PREV, GO_NEXT } from './users.actions';
+
+const initialState = {
+    usersList: [
+        {
+            age: 21,
+            name: 'Bob',
+            id: 'id-0',
+        },
+        {
+            age: 17,
+            name: 'Tom',
+            id: 'id-1',
+        },
+        {
+            age: 18,
+            name: 'Tad',
+            id: 'id-2',
+        },
+        {
+            age: 45,
+            name: 'Sam',
+            id: 'id-3',
+        },
+        {
+            age: 12,
+            name: 'Gus',
+            id: 'id-0',
+        },
+        {
+            age: 60,
+            name: 'Ozzy',
+            id: 'id-1',
+        },
+        {
+            age: 28,
+            name: 'Dock',
+            id: 'id-2',
+        },
+        {
+            age: 49,
+            name: 'Zak',
+            id: 'id-3',
+        }
+
+
+    ],
+
+    currentPage: 0,
+
+};
+
+const usersReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case GO_PREV:
+            return {
+                ...state,
+                currentPage: state.currentPage - 1,
+            };
+
+
+        case GO_NEXT: {
+
+            return {
+                ...state,
+                currentPage: state.currentPage + 1,
+            };
+        }
+
+
+        default:
+            return state;
+    }
+}
+
+export default usersReducer;
