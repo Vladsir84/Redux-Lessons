05b057fd5df0fd767465b9b1b3370b556185e75c
John Doe <vperedz@gmail.com>
2020-06-12 15:24:13 +0300

    add

--- lesson4/task3/src/users/Pagination.jsx
+++ lesson4/task3/src/users/Pagination.jsx
@@ -7,25 +7,25 @@ const Pagination = ({
     totalItems,
     itemsPerPage,
 }) => {
-    const prevPageAvailable = currentPage > 0;
-    const nextPageAvailable =
+    const isPrevPageAvailable = currentPage > 0;
+    const isNextPageAvailable =
         currentPage < Math.ceil(totalItems / itemsPerPage) - 1;
     return (
         <div className="pagination">
             <button
                 className="btn"
                 onClick={goPrev}
-                disabled={!prevPageAvailable}
+                disabled={!isPrevPageAvailable}
             >
-                {prevPageAvailable && '←'}
+                {isPrevPageAvailable && '←'}
             </button>
             <span className="pagination__page">{currentPage + 1}</span>
             <button
                 className="btn"
                 onClick={goNext}
-                disabled={!nextPageAvailable}
+                disabled={!isNextPageAvailable}
             >
-                {nextPageAvailable && '→'}
+                {isNextPageAvailable && '→'}
             </button>
         </div>
     );
