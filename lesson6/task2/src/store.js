aca77b69fdf9dc4a917187020cd45e05d5ae2b79
John Doe <vperedz@gmail.com>
2020-06-14 17:57:02 +0300

    add

--- lesson6/task2/src/store.js
+++ lesson6/task2/src/store.js
@@ -8,10 +8,19 @@ const reducer = combineReducers({
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const logger = store => next => action  => {
+    console.group(action.type);
+    console.info('dispatching', action);
+    let result = next(action);
+    console.log('next state', store.getState());
+    console.groupEnd();
+    return result;
+};
+
 const store = createStore(
     reducer,
     composeEnhancers(
-        applyMiddleware(thunk),
+        applyMiddleware(thunk, logger),
     )
 );
 
